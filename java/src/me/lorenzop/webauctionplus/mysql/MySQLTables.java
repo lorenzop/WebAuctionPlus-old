6edc259c5feec895929da88fec4dbc73f5b80181
lorenzo42p <lorenzo42p@c41ed8a6-1c15-8a3d-a996-777bd911872f>
2012-04-30 08:27:20 +0000

    moving database generation and updating code to a separate class so it can be unloaded
    git-svn-id: http://webauctionplus.googlecode.com/svn/trunk@80 c41ed8a6-1c15-8a3d-a996-777bd911872f

---
+++ java/src/me/lorenzop/webauctionplus/mysql/MySQLTables.java
@@ -0,0 +1,17 @@
+package me.lorenzop.webauctionplus.mysql;
+
+import me.lorenzop.webauctionplus.WebAuctionPlus;
+
+public class MySQLTables {
+
+	protected final WebAuctionPlus plugin;
+
+	public MySQLTables(WebAuctionPlus plugin) {
+		this.plugin = plugin;
+	}
+
+
+
+
+
+}
