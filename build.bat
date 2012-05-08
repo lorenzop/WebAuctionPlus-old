c509214c98610d24c754ed459331fb36c5b13d9c
lorenzo42p <lorenzo42p@c41ed8a6-1c15-8a3d-a996-777bd911872f>
2012-05-08 03:46:42 +0000

    added build.bat to prepare and compress final zip package finished WebAuctionPlus 1.0.3
    git-svn-id: http://webauctionplus.googlecode.com/svn/trunk@91 c41ed8a6-1c15-8a3d-a996-777bd911872f

---
+++ build.bat
@@ -0,0 +1,46 @@
+@echo off
+cls
+set tempdir=\WebAuctionPlus
+set outputfile=%CD%\WebAuctionPlus-1.0.xalpha.zip
+
+
+IF EXIST %tempdir% (
+  echo temp dir already exists!
+  pause
+  exit
+)
+IF EXIST %outputfile% (
+  echo output file already exists!
+  pause
+  exit
+)
+
+
+echo output file %outputfile%
+echo.
+mkdir %tempdir%
+
+
+echo Making copy to %tempdir% ..
+echo.
+mkdir %tempdir%\webauctionplus
+xcopy .\www\WebInterface %tempdir%\webauctionplus /s /e
+copy .\WebAuctionPlus*.jar %tempdir%
+
+
+echo Compressing files..
+echo.
+zip.exe -r "%outputfile%" "%tempdir%"
+echo.
+echo.
+
+
+echo.
+echo Finished!
+pause
+
+
+echo.
+echo Removing temp webauctionplus folder..
+echo.
+rmdir /S /Q "%tempdir%"
