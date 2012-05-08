f0d797f3e3aaade36b9bf8303d069076b82ca6c2
lorenzo42p <lorenzo42p@c41ed8a6-1c15-8a3d-a996-777bd911872f>
2012-05-08 00:16:34 +0000

    minor cleanup of mcface.php
    git-svn-id: http://webauctionplus.googlecode.com/svn/trunk@89 c41ed8a6-1c15-8a3d-a996-777bd911872f

--- www/WebInterface/inc/mcface.php
+++ www/WebInterface/inc/mcface.php
@@ -1,30 +1,29 @@
 <?php
 
 
-$username=@$_GET['username'];
-$username=str_replace('\\','',str_replace('/','',htmlspecialchars($username)));
+$username = @$_GET['username'];
+$username = str_replace('\\', '', str_replace('/', '', htmlspecialchars($username) ));
 // image cache control
-image_cache_control(substr(@$_SERVER['SCRIPT_NAME'],strrpos(@$_SERVER['SCRIPT_NAME'],'/')+1));
+image_cache_control( substr(@$_SERVER['SCRIPT_NAME'], strrpos(@$_SERVER['SCRIPT_NAME'],'/')+1) );
 // get image
-$image=@file_get_contents('http://minotar.net/avatar/'.$username);
+$image = @file_get_contents('http://minotar.net/avatar/'.$username);
 // load default face
-if($image==FALSE){
-  $image=file_get_contents('images/default_face.png');}
+if(empty($image)) $image = file_get_contents('images/default_face.png');
 header('Content-type: image/png');
 echo $image;
 exit();
 
 // image cache control
 function image_cache_control($file){
-  $expire_time=10800;
-  $filemtime=filemtime($file);
+  $expire_time = 10800;
+  $filemtime = filemtime($file);
   // browser cache 3 hours
   header('Cache-Control: private, max-age='.$expire_time.', pre-check='.$expire_time);
   header('Pragma: private');
-  header('Expires: '.date(DATE_RFC822,time()+$expire_time));
+  header('Expires: '.date(DATE_RFC822,time() + $expire_time));
   if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) &&
     strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE'])==$filemtime &&
-    time()-strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE'])<$expire_time ){
+    time()-strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) < $expire_time ){
     // send the last mod time of the file back
     header('Last-Modified: '.gmdate('D, d M Y H:i:s', $filemtime).' GMT', true, 304);
     header('Content-type: image/png');
