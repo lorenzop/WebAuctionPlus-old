8553dca49739b42f2d0f7c945aecf40765d35945
lorenzo42p <lorenzo42p@c41ed8a6-1c15-8a3d-a996-777bd911872f>
2012-07-28 09:03:36 +0000

    added Tube Stuff minor fixes to Red Power
    git-svn-id: http://webauctionplus.googlecode.com/svn/trunk@194 c41ed8a6-1c15-8a3d-a996-777bd911872f

---
+++ www/WebInterface/inc/ItemPacks/TubeStuff/item.defines.php
@@ -0,0 +1,26 @@
+<?php if(!defined('DEFINE_INDEX_FILE')){if(headers_sent()){echo '<header><meta http-equiv="refresh" content="0;url=../"></header>';}else{header('HTTP/1.0 301 Moved Permanently'); header('Location: ../');} die("<font size=+2>Access Denied!!</font>");}
+// Tube Stuff - item definitions
+$Items = &ItemFuncs::$Items;
+$pack  = 'TubeStuff';
+
+$Items[194]=array(
+  0 =>array(
+    'name'=>'Buffer',
+    'icon'=>'Buffer.png',
+    'pack'=>$pack),
+  1 =>array(
+    'name'=>'Auto Crafting Table III',
+    'icon'=>'Automatic_Crafting_Table_MkIII.png',
+    'pack'=>$pack),
+  2 =>array(
+    'name'=>'Black Hole Chest',
+    'icon'=>'Black_Hole_Chest.png',
+    'pack'=>$pack),
+  -1=>array(
+    'name'=>'Unknown Item',
+    'icon'=>'Automatic_Crafting_Table_MkIII.png',
+    'pack'=>$pack),
+);
+
+unset($Items);
+?>
