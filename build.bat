6119dd4930a1a5c18c421aab4469b253fa0a39cc
lorenzo42p <lorenzo42p@c41ed8a6-1c15-8a3d-a996-777bd911872f>
2012-08-09 17:44:53 +0000

    compiled 1.1.2 updated build.bat to zip ItemPacks separate
    git-svn-id: http://webauctionplus.googlecode.com/svn/trunk@224 c41ed8a6-1c15-8a3d-a996-777bd911872f

--- build.bat
+++ build.bat
@@ -3,9 +3,17 @@ cls
 set tempdir=\WebAuctionPlus
 set outputfile=%CD%\WebAuctionPlus-1.1.xalpha.zip
 
+set tempdiritem=\ItemPacks
+set outputfileitem=%tempdir%\AdditionalItemPacks.zip
+
 
 IF EXIST %tempdir% (
   echo temp dir already exists!
+  pause
+  exit
+)
+IF EXIST %tempdiritem% (
+  echo items temp dir already exists!
   pause
   exit
 )
@@ -19,18 +27,40 @@ IF EXIST %outputfile% (
 echo output file %outputfile%
 echo.
 mkdir %tempdir%
+echo.
+echo.
 
 
 echo Making copy to %tempdir% ..
 echo.
 mkdir %tempdir%\webauctionplus
 xcopy .\www\WebInterface %tempdir%\webauctionplus /s /e /H
+rmdir /S /Q %tempdir%\webauctionplus\resources
 copy .\WebAuctionPlus*.jar %tempdir%
+echo.
+echo.
 
 
 copy .\README %tempdir%
 rename %tempdir%\README README.txt
 copy .\changelog.txt %tempdir%
+echo.
+echo.
+
+
+echo Making items copy to %tempdiritem% ..
+echo.
+mkdir %tempdiritem%
+xcopy .\ItemPacks %tempdiritem% /s /e /H
+echo.
+echo.
+
+
+echo Compressing item packs..
+echo.
+zip.exe -r "%outputfileitem%" "%tempdiritem%"
+echo.
+echo.
 
 
 echo Compressing files..
@@ -43,9 +73,14 @@ echo.
 echo.
 echo Finished!
 pause
-
-
 echo.
+echo.
+
+
 echo Removing temp webauctionplus folder..
 echo.
 rmdir /S /Q "%tempdir%"
+rmdir /S /Q "%tempdiritem%"
+echo.
+echo.
+
