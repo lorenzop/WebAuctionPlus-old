0f3806bd4fb8f0023426b5d02aa737d277224dca
lorenzo42p <lorenzo42p@c41ed8a6-1c15-8a3d-a996-777bd911872f>
2013-02-05 15:59:33 +0000

    updated plugin.yml file   removed SignLink softdependency updated bukkit 1.4.7 and vault 1.2.22 resources
    git-svn-id: http://webauctionplus.googlecode.com/svn/trunk@313 c41ed8a6-1c15-8a3d-a996-777bd911872f

--- java/src/com/webauctionplus/waSettings.java
+++ java/src/com/webauctionplus/waSettings.java
@@ -1,176 +1,171 @@
-package me.lorenzop.webauctionplus;
+package com.webauctionplus;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.HashMap;
 
-import me.lorenzop.webauctionplus.mysql.MySQLPoolConn;
 
 public class waSettings {
 
-	protected HashMap<String, String> settingsMap = new HashMap<String, String>();
+//	protected HashMap<String, String> settingsMap = new HashMap<String, String>();
 
-	private final WebAuctionPlus plugin;
-	private boolean isOk;
+//	private final WebAuctionPlus plugin;
+//	private boolean isOk;
 
-	public waSettings(WebAuctionPlus plugin) {
-		this.plugin = plugin;
-		isOk = false;
-	}
-
-
-	public synchronized void LoadSettings(){
-		isOk = false;
-		MySQLPoolConn poolConn = WebAuctionPlus.dbPool.getLock();
-		PreparedStatement st = null;
-		ResultSet rs = null;
-		try {
-			if(WebAuctionPlus.isDebug()) WebAuctionPlus.log.info("WA Query: LoadSettings");
-			st = poolConn.getConn().prepareStatement("SELECT `name`, `value` FROM `"+poolConn.dbPrefix()+"Settings`");
-			rs = st.executeQuery();
-			while (rs.next()) {
-				if(rs.getString(1) != null)
-					settingsMap.put(rs.getString(1), rs.getString(2));
-			}
-			updateSettingsTable();
-		} catch (SQLException e) {
-			WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix + "Unable to get settings");
-			e.printStackTrace();
-			return;
-		} finally {
-			poolConn.releaseLock(st, rs);
-			poolConn = null;
-		}
-		addDefaults();
-		WebAuctionPlus.log.info(WebAuctionPlus.logPrefix+"Loaded "+Integer.toString(settingsMap.size())+" settings from db");
-		isOk = (settingsMap.size()!=0);
-	}
-	public boolean isOk() {return this.isOk;}
-
-
-	// set default settings
-	private void addDefaults() {
-		addDefault("Version",				plugin.getDescription().getVersion().toString());
-		addDefault("Language",				"en");
-		addDefault("Require Login",			false);
-		addDefault("ez Login",				true);
-		addDefault("CSRF Protection",		true);
-		addDefault("Currency Prefix",		"$ ");
-		addDefault("Currency Postfix",		"");
-		addDefault("Custom Description",	false);
-		addDefault("Inventory Rows",		6);
-		addDefault("Website Theme",			"");
-		addDefault("jQuery UI Pack",		"");
-		addDefault("Item Packs",			"");
-		addDefault("Max Sell Price",		10000.00);
-//		addDefault("Max Selling Per Player",20);
-//		addDefault("Storage base per stack",1.0);
-//		addDefault("Storage add per item",	0.1);
-	}
-	private void addDefault(String name, String value) {
-		if(!settingsMap.containsKey(name)) {
-//			if (plugin.dataQueries.debugSQL) WebAuctionPlus.log.info("WA Query: Insert setting: " + name);
-			WebAuctionPlus.log.info(WebAuctionPlus.logPrefix + "Adding default setting for: " + name);
-			MySQLPoolConn poolConn = WebAuctionPlus.dbPool.getLock();
-			PreparedStatement st = null;
-			ResultSet rs = null;
-			try {
-				st = poolConn.getConn().prepareStatement("INSERT INTO `"+poolConn.dbPrefix()+"Settings` (`name`,`value`) VALUES (?, ?)");
-				st.setString(1, name);
-				st.setString(2, value);
-				st.executeUpdate();
-				settingsMap.put(name, value);
-			} catch (SQLException e) {
-				WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix+"Unable to add setting: "+name);
-				e.printStackTrace();
-			} finally {
-				poolConn.releaseLock(st, rs);
-				poolConn = null;
-			}
-		}
-	}
-	private void addDefault(String name, boolean value) {
-		if(value) addDefault(name, "true");
-		else      addDefault(name, "false");
-	}
-	private void addDefault(String name, int value) {
-		addDefault(name, Integer.toString(value));
-	}
-//	private void addDefault(String name, long value) {
-//		addDefault(name, Long.toString(value));
+//	public waSettings(WebAuctionPlus plugin) {
+//		this.plugin = plugin;
+//		isOk = false;
 //	}
-	private void addDefault(String name, double value) {
-		addDefault(name, Double.toString(value));
-	}
 
 
-	private void updateSettingsTable() {
-		if(settingsMap.containsKey("jquery ui pack")) {
-			WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix+"Updating Settings table: jQuery UI Pack");
-			WebAuctionPlus.dbPool.executeRawSQL("UPDATE `"+WebAuctionPlus.dbPool.dbPrefix()+"Settings` SET `name` = 'jQuery UI Pack' WHERE `name` = 'jquery ui pack' LIMIT 1");
-			settingsMap.put("jQuery UI Pack", "");
-		}
-	}
+//	public synchronized void LoadSettings(){
+//		isOk = false;
+//		MySQLPoolConn poolConn = WebAuctionPlus.getDB();
+//		PreparedStatement st = null;
+//		ResultSet rs = null;
+//		try {
+//			if(WebAuctionPlus.isDebug()) WebAuctionPlus.log.info("WA Query: LoadSettings");
+//			st = poolConn.getConn().prepareStatement("SELECT `name`, `value` FROM `"+poolConn.dbPrefix()+"Settings`");
+//			rs = st.executeQuery();
+//			while (rs.next()) {
+//				if(rs.getString(1) != null)
+//					settingsMap.put(rs.getString(1), rs.getString(2));
+//			}
+//			updateSettingsTable();
+//		} catch (SQLException e) {
+//			WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix + "Unable to get settings");
+//			e.printStackTrace();
+//			return;
+//		} finally {
+//			poolConn.releaseLock(st, rs);
+//			poolConn = null;
+//		}
+//		addDefaults();
+//		WebAuctionPlus.log.info(WebAuctionPlus.logPrefix+"Loaded "+Integer.toString(settingsMap.size())+" settings from db");
+//		isOk = (settingsMap.size()!=0);
+//	}
+//	public boolean isOk() {return this.isOk;}
 
 
-	// get setting
-	public synchronized String getString(String name) {
-		if(settingsMap.containsKey(name))
-			return settingsMap.get(name);
-		else
-			return null;
-	}
-	public boolean getBoolean(String name) {
-		String value = this.getString(name);
-		if(     value.equalsIgnoreCase("true"))		return true;
-		else if(value.equalsIgnoreCase("false"))	return false;
-		else if(value.equalsIgnoreCase("on"))		return true;
-		else if(value.equalsIgnoreCase("off"))		return false;
-		else										return Boolean.valueOf(value);
-	}
-	public int getInteger(String name) {
-		return Integer.valueOf(this.getString(name));
-	}
-	public double getDouble(String name) {
-		return Double.valueOf(this.getString(name));
-	}
+//	// set default settings
+//	private void addDefaults() {
+//		addDefault("Version",				plugin.getDescription().getVersion().toString());
+//		addDefault("Language",				"en");
+//		addDefault("Require Login",			false);
+//		addDefault("ez Login",				true);
+//		addDefault("CSRF Protection",		true);
+//		addDefault("Currency Prefix",		"$ ");
+//		addDefault("Currency Postfix",		"");
+//		addDefault("Custom Description",	false);
+//		addDefault("Inventory Rows",		6);
+//		addDefault("Website Theme",			"");
+//		addDefault("jQuery UI Pack",		"");
+//		addDefault("Item Packs",			"");
+//		addDefault("Max Sell Price",		10000.00);
+////		addDefault("Max Selling Per Player",20);
+////		addDefault("Storage base per stack",1.0);
+////		addDefault("Storage add per item",	0.1);
+//	}
+//	private void addDefault(String name, String value) {
+//		if(!settingsMap.containsKey(name)) {
+////			if (plugin.dataQueries.debugSQL) WebAuctionPlus.log.info("WA Query: Insert setting: " + name);
+//			WebAuctionPlus.log.info(WebAuctionPlus.logPrefix + "Adding default setting for: " + name);
+//			MySQLPoolConn poolConn = WebAuctionPlus.dbPool.getLock();
+//			PreparedStatement st = null;
+//			ResultSet rs = null;
+//			try {
+//				st = poolConn.getConn().prepareStatement("INSERT INTO `"+poolConn.dbPrefix()+"Settings` (`name`,`value`) VALUES (?, ?)");
+//				st.setString(1, name);
+//				st.setString(2, value);
+//				st.executeUpdate();
+//				settingsMap.put(name, value);
+//			} catch (SQLException e) {
+//				WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix+"Unable to add setting: "+name);
+//				e.printStackTrace();
+//			} finally {
+//				poolConn.releaseLock(st, rs);
+//				poolConn = null;
+//			}
+//		}
+//	}
+//	private void addDefault(String name, boolean value) {
+//		if(value) addDefault(name, "true");
+//		else      addDefault(name, "false");
+//	}
+//	private void addDefault(String name, int value) {
+//		addDefault(name, Integer.toString(value));
+//	}
+////	private void addDefault(String name, long value) {
+////		addDefault(name, Long.toString(value));
+////	}
+//	private void addDefault(String name, double value) {
+//		addDefault(name, Double.toString(value));
+//	}
 
 
-	// change setting
-	public synchronized void setString(String name, String value) {
-		if(!settingsMap.containsKey(name)) {
-			WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix+"Setting not found! "+name);
-			return;
-		}
-		if(settingsMap.get(name).equals(value)) {
-			WebAuctionPlus.log.info(WebAuctionPlus.logPrefix+"Setting unchanged, matches existing. "+name);
-			return;
-		}
-		settingsMap.put(name, value);
-		MySQLPoolConn poolConn = WebAuctionPlus.dbPool.getLock();
-		PreparedStatement st = null;
-		ResultSet rs = null;
-		if (WebAuctionPlus.isDebug()) WebAuctionPlus.log.info("WA Query: Update setting: " + name);
-		try {
-			st = poolConn.getConn().prepareStatement("UPDATE `"+poolConn.dbPrefix()+"Settings` SET `value` = ? WHERE `name` = ? LIMIT 1");
-			st.setString(1, value);
-			st.setString(2, name);
-			st.executeUpdate();
-		} catch(SQLException e) {
-			WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix + "Unable to update setting " + name);
-			e.printStackTrace();
-		} finally {
-			poolConn.releaseLock(st, rs);
-			poolConn = null;
-		}
-	}
-	public void setInteger(String name, int value) {
-		this.setString(name, Integer.toString(value));
-	}
-	public void setBoolean(String name, boolean value) {
-		this.setString(name, Boolean.toString(value));
-	}
+//	private void updateSettingsTable() {
+//		if(settingsMap.containsKey("jquery ui pack")) {
+//			WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix+"Updating Settings table: jQuery UI Pack");
+//			WebAuctionPlus.dbPool.executeRawSQL("UPDATE `"+WebAuctionPlus.dbPool.dbPrefix()+"Settings` SET `name` = 'jQuery UI Pack' WHERE `name` = 'jquery ui pack' LIMIT 1");
+//			settingsMap.put("jQuery UI Pack", "");
+//		}
+//	}
+
+
+//	// get setting
+//	public synchronized String getString(String name) {
+//		if(settingsMap.containsKey(name))
+//			return settingsMap.get(name);
+//		else
+//			return null;
+//	}
+//	public boolean getBoolean(String name) {
+//		String value = this.getString(name);
+//		if(     value.equalsIgnoreCase("true"))		return true;
+//		else if(value.equalsIgnoreCase("false"))	return false;
+//		else if(value.equalsIgnoreCase("on"))		return true;
+//		else if(value.equalsIgnoreCase("off"))		return false;
+//		else										return Boolean.valueOf(value);
+//	}
+//	public int getInteger(String name) {
+//		return Integer.valueOf(this.getString(name));
+//	}
+//	public double getDouble(String name) {
+//		return Double.valueOf(this.getString(name));
+//	}
+
+
+//	// change setting
+//	public synchronized void setString(String name, String value) {
+//		if(!settingsMap.containsKey(name)) {
+//			WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix+"Setting not found! "+name);
+//			return;
+//		}
+//		if(settingsMap.get(name).equals(value)) {
+//			WebAuctionPlus.log.info(WebAuctionPlus.logPrefix+"Setting unchanged, matches existing. "+name);
+//			return;
+//		}
+//		settingsMap.put(name, value);
+//		MySQLPoolConn poolConn = WebAuctionPlus.dbPool.getLock();
+//		PreparedStatement st = null;
+//		ResultSet rs = null;
+//		if (WebAuctionPlus.isDebug()) WebAuctionPlus.log.info("WA Query: Update setting: " + name);
+//		try {
+//			st = poolConn.getConn().prepareStatement("UPDATE `"+poolConn.dbPrefix()+"Settings` SET `value` = ? WHERE `name` = ? LIMIT 1");
+//			st.setString(1, value);
+//			st.setString(2, name);
+//			st.executeUpdate();
+//		} catch(SQLException e) {
+//			WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix + "Unable to update setting " + name);
+//			e.printStackTrace();
+//		} finally {
+//			poolConn.releaseLock(st, rs);
+//			poolConn = null;
+//		}
+//	}
+//	public void setInteger(String name, int value) {
+//		this.setString(name, Integer.toString(value));
+//	}
+//	public void setBoolean(String name, boolean value) {
+//		this.setString(name, Boolean.toString(value));
+//	}
 
 
 }
---
+++ java/src/com/webauctionplus/waStatsCache.java
@@ -0,0 +1,102 @@
+package com.webauctionplus;
+
+
+public class waStatsCache {
+
+//	// long cycle stats
+//	private int  totalBuyNowCount	= 0;
+//	private int  totalAuctionCount	= 0;
+//	private int  maxAuctionId		=-1;
+
+//	private long lastUpdate =-1;
+
+
+//	public waStatsCache() {
+//	}
+
+
+//	private synchronized boolean Update() {
+//		long tim = pxnUtils.getCurrentMillis();
+//		// update no more than every 5 seconds
+//		if( lastUpdate == -1 || ((tim-lastUpdate) >= (5000)) ) {
+//			doUpdate();
+//			lastUpdate = tim;
+//			return true;
+//		}
+//		return false;
+//	}
+
+
+//	private void doUpdate() {
+//		if(WebAuctionPlus.isDebug()) WebAuctionPlus.log.info(WebAuctionPlus.logPrefix+"Updating stats");
+//		MySQLPoolConn poolConn = WebAuctionPlus.dbPool.getLock();
+//		PreparedStatement st = null;
+//		ResultSet rs = null;
+//
+//		// total buy nows
+//		totalBuyNowCount = 0;
+//		try {
+//			if(WebAuctionPlus.isDebug()) WebAuctionPlus.log.info("WA Query: Stats::count buy nows");
+//			st = poolConn.getConn().prepareStatement("SELECT COUNT(*) FROM `"+poolConn.dbPrefix()+"Auctions` WHERE `allowBids` = 0");
+//			rs = st.executeQuery();
+//			if(rs.next()) totalBuyNowCount = rs.getInt(1);
+//		} catch (SQLException e) {
+//			WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix + "Unable to get total buy now count");
+//			e.printStackTrace();
+//		} finally {
+//			poolConn.freeResource(st, rs);
+//		}
+//
+//		// total auctions
+//		totalAuctionCount = 0;
+//		try {
+//			st = null;
+//			rs = null;
+//			if(WebAuctionPlus.isDebug()) WebAuctionPlus.log.info("WA Query: Stats::count auctions");
+//			st = poolConn.getConn().prepareStatement("SELECT COUNT(*) FROM `"+poolConn.dbPrefix()+"Auctions` WHERE `allowBids` != 0");
+//			rs = st.executeQuery();
+//			if(rs.next()) totalAuctionCount = rs.getInt(1);
+//		} catch (SQLException e) {
+//			WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix + "Unable to get total auction count");
+//			e.printStackTrace();
+//		} finally {
+//			poolConn.freeResource(st, rs);
+//		}
+//
+//		// get max auction id
+//		maxAuctionId = -1;
+//		try {
+//			st = null;
+//			rs = null;
+//			if(WebAuctionPlus.isDebug()) WebAuctionPlus.log.info("WA Query: Stats::getMaxAuctionID");
+//			st = poolConn.getConn().prepareStatement("SELECT MAX(`id`) AS `id` FROM `"+poolConn.dbPrefix()+"Auctions`");
+//			rs = st.executeQuery();
+//			if(rs.next()) maxAuctionId = rs.getInt("id");
+//		} catch (SQLException e) {
+//			WebAuctionPlus.log.warning(WebAuctionPlus.logPrefix + "Unable to query for max Auction ID");
+//			e.printStackTrace();
+//		} finally {
+//			poolConn.freeResource(st, rs);
+//		}
+//
+//		poolConn.releaseLock();
+//		poolConn = null;
+//	}
+
+
+//	// data access layer
+//	public int getTotalBuyNows() {
+//		Update();
+//		return totalBuyNowCount;
+//	}
+//	public int getTotalAuctions() {
+//		Update();
+//		return totalAuctionCount;
+//	}
+//	public int getMaxAuctionID() {
+//		Update();
+//		return maxAuctionId;
+//	}
+
+
+}
