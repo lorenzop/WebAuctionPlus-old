56a1fa2c373da62ad000f62b2224ba4cf9035a98
lorenzo42p <lorenzo42p@c41ed8a6-1c15-8a3d-a996-777bd911872f>
2013-02-25 06:12:32 +0000

    added item and crafting defines files for categories
    git-svn-id: http://webauctionplus.googlecode.com/svn/trunk@339 c41ed8a6-1c15-8a3d-a996-777bd911872f

---
+++ www/ItemPacks/default/Building/crafting.defines.php
@@ -0,0 +1,11 @@
+<?php namespace psm;
+if(!defined('DEFINE_INDEX_FILE')){if(headers_sent()){echo '<header><meta http-equiv="refresh" content="0;url=../"></header>';}else{header('HTTP/1.0 301 Moved Permanently'); header('Location: ../');} die("<font size=+2>Access Denied!!</font>");}
+// Building - item crafting recipes
+abstract class Crafting_Default_Building extends CraftingDefinesFile {
+
+
+
+
+
+}
+?>
