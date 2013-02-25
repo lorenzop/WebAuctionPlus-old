cb185428dd792aba2116db5983c512d2abad1db3
lorenzo42p <lorenzo42p@c41ed8a6-1c15-8a3d-a996-777bd911872f>
2013-02-25 16:57:09 +0000

    added items and crafting class files for default item pack
    git-svn-id: http://webauctionplus.googlecode.com/svn/trunk@343 c41ed8a6-1c15-8a3d-a996-777bd911872f

---
+++ www/ItemPacks/default/items.php
@@ -0,0 +1,28 @@
+<?php namespace psm\ItemDefines;
+if(!defined('PORTAL_INDEX_FILE')){if(headers_sent()){echo '<header><meta http-equiv="refresh" content="0;url=../"></header>';}else{header('HTTP/1.0 301 Moved Permanently'); header('Location: ../');} die("<font size=+2>Access Denied!!</font>");}
+class DefinesLoader_default_items extends DefinesLoader {
+
+
+	public function __construct() {
+		$this->LoadCategory('Brewing');
+		$this->LoadCategory('Building');
+		$this->LoadCategory('Combat');
+		$this->LoadCategory('Decoration');
+		$this->LoadCategory('Foodstuffs');
+		$this->LoadCategory('Materials');
+		$this->LoadCategory('Misc');
+		$this->LoadCategory('Redstone');
+		$this->LoadCategory('Special');
+		$this->LoadCategory('Transport');
+	}
+
+
+	protected function getPath() {
+		return 'ItemPacks/default/';
+	}
+	protected function getType() {
+		return 'items';
+	}
+
+}
+?>
