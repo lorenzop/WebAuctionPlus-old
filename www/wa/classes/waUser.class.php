5059086a5d7bfbc27ae2d35c3533dcd9e0b5f5f2
lorenzo42p <lorenzo42p@c41ed8a6-1c15-8a3d-a996-777bd911872f>
2013-03-03 10:00:33 +0000

    updated index define checks
    git-svn-id: http://webauctionplus.googlecode.com/svn/trunk@353 c41ed8a6-1c15-8a3d-a996-777bd911872f

--- www/wa/classes/waUser.class.php
+++ www/wa/classes/waUser.class.php
@@ -1,5 +1,5 @@
 <?php namespace wa;
-if(!defined('PORTAL_INDEX_FILE') || \PORTAL_INDEX_FILE!==TRUE){if(headers_sent()){echo '<header><meta http-equiv="refresh" content="0;url=../"></header>';}else{header('HTTP/1.0 301 Moved Permanently'); header('Location: ../');} die("<font size=+2>Access Denied!!</font>");}
+if(!defined('psm\INDEX_FILE') || \psm\INDEX_FILE!==TRUE) {if(headers_sent()) {echo '<header><meta http-equiv="refresh" content="0;url=../"></header>';} else {header('HTTP/1.0 301 Moved Permanently'); header('Location: ../');} die("<font size=+2>Access Denied!!</font>");}
 class waUser extends \psm\User {
 
 	// wa player
--- www/wa/html/default/main.html.php
+++ www/wa/html/default/main.html.php
@@ -6,7 +6,7 @@ function GetRenderTime() {
 }
 
 
-if(!defined('PORTAL_INDEX_FILE') || \PORTAL_INDEX_FILE!==TRUE){if(headers_sent()){echo '<header><meta http-equiv="refresh" content="0;url=../"></header>';}else{header('HTTP/1.0 301 Moved Permanently'); header('Location: ../');} die("<font size=+2>Access Denied!!</font>");}
+if(!defined('psm\INDEX_FILE') || \psm\INDEX_FILE!==TRUE) {if(headers_sent()) {echo '<header><meta http-equiv="refresh" content="0;url=../"></header>';} else {header('HTTP/1.0 301 Moved Permanently'); header('Location: ../');} die("<font size=+2>Access Denied!!</font>");}
 class html_main extends \psm\html\tplFile_Main {
 
 	private $mainMenu;
